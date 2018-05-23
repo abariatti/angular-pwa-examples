87023aae27a0d23d3e0c7ed3fdd0266ab18a0f63
Bariatti Arnaud <Arnaud.Bariatti@swisscom.com>
2018-05-23 16:04:18 +0200

    added scanner + service workers

--- pwa-scanner/src/app/app.module.ts
+++ pwa-scanner/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { MediaStreamComponent } from './media-stream/media-stream.component';
 import { BarcodeValidatorService } from '../services/barcode-validator.service';
 import { BarcodeDecoderService } from '../services/barcode-decoder.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { BarcodeDecoderService } from '../services/barcode-decoder.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AppMaterialModule
+    AppMaterialModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     BarcodeValidatorService,
